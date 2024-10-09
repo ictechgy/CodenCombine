8745ca48041db12a43b8e45559df066cf688a3f2
ictechgy <ictechgy@gmail.com>
2024-10-09 12:57:52 +0900

    refactor: DispatchQueue extension 파일 분리

---
+++ CodenCombine/Publishers+Extension/DispatchQueue.swift
@@ -0,0 +1,26 @@
+//
+//  DispatchQueue.swift
+//  CodenCombine
+//
+//  Created by JINHONG AN on 10/9/24.
+//
+
+import Foundation
+
+extension DispatchQueue {
+    private static let defaultKey = DispatchSpecificKey<UUID>()
+    
+    var id: UUID {
+        if let uuid = getSpecific(key: Self.defaultKey) {
+            return uuid
+        } else {
+            let uuid = UUID()
+            setSpecific(key: Self.defaultKey, value: uuid)
+            return uuid
+        }
+    }
+    
+    static var currentRunningQueueId: UUID? {
+        getSpecific(key: Self.defaultKey)
+    }
+}
