a27cc1a23d32ce46a300e2a1c7c559bc49dd0163
ictechgy <ictechgy@gmail.com>
2024-10-11 01:24:12 +0900

    feat: ValueAccessibleAnyPublisher 구현

--- CodenCombine/Extension/CurrentValueSubject+Extension.swift
+++ CodenCombine/Extension/CurrentValueSubject+Extension.swift
@@ -7,12 +7,31 @@
 
 import Combine
 
-public protocol ValueAccessiblePublisher<Output>: Publisher, AnyObject {
+public protocol ValueAccessiblePublisher: AnyObject where Self: Publisher {
     var value: Output { get }
 }
 
 extension CurrentValueSubject: ValueAccessiblePublisher {
-    public func eraseToValueAccessiblePublisher() -> ValueAccessiblePublisher<Output> {
-        self
+    public func eraseToValueAccessibleAnyPublisher() -> ValueAccessibleAnyPublisher<CurrentValueSubject> {
+        ValueAccessibleAnyPublisher(wrappedPublisher: self)
+    }
+}
+
+public struct ValueAccessibleAnyPublisher<Wrapped: ValueAccessiblePublisher>: Publisher {
+    public typealias Output = Wrapped.Output
+    public typealias Failure = Wrapped.Failure
+    
+    private let wrappedPublisher: Wrapped
+    
+    init(wrappedPublisher: Wrapped) {
+        self.wrappedPublisher = wrappedPublisher
+    }
+    
+    public var value: Output {
+        wrappedPublisher.value
+    }
+    
+    public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        wrappedPublisher.receive(subscriber: subscriber)
     }
 }
