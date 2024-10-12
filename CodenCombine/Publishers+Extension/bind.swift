34d75d800f15452a40e44622d14bd04cae0369fd
ictechgy <ictechgy@gmail.com>
2024-10-12 16:03:00 +0900

    chore: 기록용 주석 추가

--- CodenCombine/Publishers+Extension/bind.swift
+++ CodenCombine/Publishers+Extension/bind.swift
@@ -9,6 +9,7 @@ import Combine
 
 public extension Publisher {
     func bind<Injectable: Subject>(to subjects: Injectable...) -> Cancellable where Output == Injectable.Output, Failure == Injectable.Failure {
+        // subscription 전달은 없는 상태
         sink { completion in
             subjects.forEach {
                 $0.send(completion: completion)
