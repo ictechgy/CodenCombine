7baea66002e629c194e2ef4079feadaaefb12965
ictechgy <ictechgy@gmail.com>
2024-10-12 17:46:40 +0900

    feat: bind 함수 오버로딩 추가

--- CodenCombine/Publishers+Extension/bind.swift
+++ CodenCombine/Publishers+Extension/bind.swift
@@ -20,4 +20,17 @@ public extension Publisher {
             }
         }
     }
+    
+    func bind<Receivable: Subscriber>(to subscribers: Receivable...) -> Cancellable where Output == Receivable.Input, Failure == Receivable.Failure {
+        // subscription 전달은 없는 형태
+        sink { completion in
+            subscribers.forEach {
+                $0.receive(completion: completion)
+            }
+        } receiveValue: { value in
+            subscribers.forEach {
+                _ = $0.receive(value)
+            }
+        }
+    }
 }
