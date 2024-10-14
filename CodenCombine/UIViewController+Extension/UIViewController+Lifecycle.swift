ff8d8378e9a9629ec90efbc26aece38bb0d9ae97
ictechgy <ictechgy@gmail.com>
2024-10-15 02:29:50 +0900

    refactor: viewWillAppear publisher 구현 중

--- CodenCombine/UIViewController+Extension/UIViewController+Lifecycle.swift
+++ CodenCombine/UIViewController+Extension/UIViewController+Lifecycle.swift
@@ -6,7 +6,41 @@
 //
 
 import UIKit
+import Combine
 
 extension UIViewController {
+    private static func swizzleViewWillAppear() {
+        let originalMethodSelector = #selector(viewWillAppear)
+        let targetMethodSelector = #selector(interceptedViewWillAppear)
+        
+        guard let originalMethod = class_getInstanceMethod(UIViewController.self, originalMethodSelector),
+              let targetMethod = class_getInstanceMethod(UIViewController.self, targetMethodSelector) else {
+            return
+        }
+        
+        method_exchangeImplementations(originalMethod, targetMethod)
+    }
     
+    @objc private func interceptedViewWillAppear(animated: Bool) {
+        self.viewWillAppear(animated)
+        
+    }
 }
+
+extension UIViewController {
+    enum AssociatedKeys {
+        static var viewWillAppear = 0
+    }
+    
+    var viewWillAppearPublisher: AnyPublisher<Void, Never>? {
+        get {
+            let associatedObject = objc_getAssociatedObject(self, &AssociatedKeys.viewWillAppear) as? PassthroughSubject<Void, Never>
+            
+            return associatedObject?.eraseToAnyPublisher()
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKeys.viewWillAppear, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+}
+
