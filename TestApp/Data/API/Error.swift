20bc0b9ee958b645add9a212a0fde14560f2cff2
ictechgy <ictechgy@gmail.com>
2024-10-20 23:02:52 +0900

    feat: 에러 타입 정의

--- TestApp/Data/API/Error.swift
+++ TestApp/Data/API/Error.swift
@@ -5,6 +5,23 @@
 //  Created by JINHONG AN on 10/20/24.
 //
 
+import Foundation
+
 enum NetworkError: Error {
     case invalidURL
+    case decodingFailed
+    case unknown
+}
+
+extension NetworkError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "유효하지 않은 요청 주소입니다."
+        case .decodingFailed:
+            return "데이터 변환에 실패하였습니다."
+        case .unknown:
+            return "알 수 없는 오류가 발생하였습니다."
+        }
+    }
 }
