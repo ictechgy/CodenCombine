446c103b98cf9b4b5e3fb456e9e8838071888b55
ictechgy <ictechgy@gmail.com>
2024-10-20 22:56:48 +0900

    feat: API 타입 구현

--- TestApp/Data/API/API.swift
+++ TestApp/Data/API/API.swift
@@ -8,6 +8,9 @@
 protocol APIProtocol {
     var baseURL: String { get }
     var defaultHeaders: [String: String] { get }
+    var additionalPath: String { get }
+    var httpMethod: HTTPMethod { get }
+    var additionalHeaders: [String: String] { get }
 }
 
 extension APIProtocol {
@@ -21,5 +24,32 @@ extension APIProtocol {
 }
 
 enum API: APIProtocol {
+    case example
     
+    var additionalPath: String {
+        switch self {
+        case .example: return ""
+        }
+    }
+    
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .example: return .get
+        }
+    }
+    
+    var additionalHeaders: [String : String] {
+        [:]
+    }
+}
+
+extension API {
+    func toEndpoint(with dataType: DataType? = nil) -> Endpoint {
+        Endpoint(
+            urlString: baseURL + additionalPath,
+            method: httpMethod,
+            headers: defaultHeaders.merging(additionalHeaders, uniquingKeysWith: { $1 }),
+            dataType: dataType
+        )
+    }
 }
