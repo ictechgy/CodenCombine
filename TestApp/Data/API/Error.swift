26f05b20f86faa0996bbcb7cb86dc100e2dea869
ictechgy <ictechgy@gmail.com>
2024-10-21 00:45:05 +0900

    feat: ImageService 구현

--- TestApp/Data/API/Error.swift
+++ TestApp/Data/API/Error.swift
@@ -10,6 +10,8 @@ import Foundation
 enum NetworkError: Error {
     case invalidURL
     case decodingFailed
+    case abnormalHTTPStatusCode(code: Int)
+    case abnormalResponse(code: Int, message: String?)
     case unknown
 }
 
@@ -20,6 +22,10 @@ extension NetworkError: LocalizedError {
             return "유효하지 않은 요청 주소입니다."
         case .decodingFailed:
             return "데이터 변환에 실패하였습니다."
+        case .abnormalHTTPStatusCode(code: let code):
+            return "불완전한 HTTP 응답 코드입니다. (\(code))"
+        case .abnormalResponse(code: let code, message: let message):
+            return "요청이 실패하였습니다. (\(code)) - \(message ?? "")"
         case .unknown:
             return "알 수 없는 오류가 발생하였습니다."
         }
--- TestApp/Data/Service/ImageService.swift
+++ TestApp/Data/Service/ImageService.swift
@@ -5,3 +5,29 @@
 //  Created by JINHONG AN on 10/20/24.
 //
 
+import Foundation
+
+final class DefaultImageService {
+    static let defaultConfiguration: URLSessionConfiguration = {
+        let configuration = URLSessionConfiguration.default
+        configuration.urlCache = URLCache(memoryCapacity: 1 * 1024 * 1024 * 1, diskCapacity: 1 * 1024 * 1024 * 1)
+        
+        return configuration
+    }()
+    
+    func request(by urlString: String, with configuration: URLSessionConfiguration = defaultConfiguration) async throws -> Data {
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession(configuration: Self.defaultConfiguration).data(from: url)
+        
+        if let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode {
+            return data
+        } else if let response = response as? HTTPURLResponse {
+            throw NetworkError.abnormalHTTPStatusCode(code: response.statusCode)
+        } else {
+            throw NetworkError.unknown
+        }
+    }
+}
