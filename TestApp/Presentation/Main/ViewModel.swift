52d74c24bd98a4b58f5486b76e8a86ac299e3401
ictechgy <ictechgy@gmail.com>
2024-10-22 02:22:27 +0900

    chore: ViewModel 기본형태 구성

---
+++ TestApp/Presentation/Main/ViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModel.swift
+//  CodenCombine
+//
+//  Created by JINHONG AN on 10/22/24.
+//
+
+final class ViewModel {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func bind(input: Input) -> Output {
+        return Output()
+    }
+}
