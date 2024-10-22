adb8a95c214b44c7a226fedfa2fd03981c4ea13b
ictechgy <ictechgy@gmail.com>
2024-10-23 00:36:26 +0900

    refactor: GestureRecognizer 부분 리팩토링

--- CodenCombine/UIView+Combine/UIView+TapGesture.swift
+++ CodenCombine/UIView+Combine/UIView+TapGesture.swift
@@ -8,32 +8,62 @@
 import UIKit
 import Combine
 
+enum GestureRecognizerType {
+    case tap
+    case pan
+    case swipe
+    
+    var instance: UIGestureRecognizer {
+        switch self {
+        case .tap:
+            return UITapGestureRecognizer()
+        case .pan:
+            return UIPanGestureRecognizer()
+        case .swipe:
+            return UISwipeGestureRecognizer()
+        }
+    }
+}
+
 public extension UIView {
+    // TODO: 함수로 구현하여 커스터마이징의 가능성을 열어주는게 좋아보임 
     var tapGesture: AnyPublisher<Void, Never> {
-        UIViewTapGeturePublisher(view: self)
+        UIViewGeturePublisher(view: self, gestureRecognizerType: .tap)
+            .eraseToAnyPublisher()
+    }
+    
+    var panGesture: AnyPublisher<Void, Never> {
+        UIViewGeturePublisher(view: self, gestureRecognizerType: .pan)
+            .eraseToAnyPublisher()
+    }
+    
+    var swipeGesture: AnyPublisher<Void, Never> {
+        UIViewGeturePublisher(view: self, gestureRecognizerType: .swipe)
             .eraseToAnyPublisher()
     }
 }
 
-struct UIViewTapGeturePublisher: Publisher {
+struct UIViewGeturePublisher: Publisher {
     typealias Output = Void
     typealias Failure = Never
     
     let view: UIView
+    let gestureRecognizerType: GestureRecognizerType
     
     func receive<S>(subscriber: S) where S: Subscriber, Failure == S.Failure, Output == S.Input {
-        let subscription = UIViewTapGestureSubscription(view: self.view, subscriber: subscriber)
+        let subscription = UIViewGestureSubscription(view: self.view, gestureRecognizerType: gestureRecognizerType, subscriber: subscriber)
         subscriber.receive(subscription: subscription)
     }
 }
 
-final class UIViewTapGestureSubscription<S: Subscriber>: Subscription where S.Input == Void, S.Failure == Never {
+final class UIViewGestureSubscription<S: Subscriber>: Subscription where S.Input == Void, S.Failure == Never {
     private weak var view: UIView?
     private var subscriber: S?
-    private var tapGestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer()
+    private var gestureRecognizer: UIGestureRecognizer
     
-    init(view: UIView, subscriber: S) {
+    init(view: UIView, gestureRecognizerType: GestureRecognizerType, subscriber: S) {
         self.view = view
+        self.gestureRecognizer = gestureRecognizerType.instance
         self.subscriber = subscriber
         registerTapGesture()
     }
@@ -43,7 +73,7 @@ final class UIViewTapGestureSubscription<S: Subscriber>: Subscription where S.In
     }
     
     func cancel() {
-        view?.removeGestureRecognizer(tapGestureRecognizer)
+        view?.removeGestureRecognizer(gestureRecognizer)
         view = nil
         subscriber = nil
     }
@@ -51,8 +81,8 @@ final class UIViewTapGestureSubscription<S: Subscriber>: Subscription where S.In
     private func registerTapGesture() {
         guard let view else { return }
         
-        tapGestureRecognizer.addTarget(self, action: #selector(onViewTapped))
-        view.addGestureRecognizer(tapGestureRecognizer)
+        gestureRecognizer.addTarget(self, action: #selector(onViewTapped))
+        view.addGestureRecognizer(gestureRecognizer)
     }
     
     @objc
