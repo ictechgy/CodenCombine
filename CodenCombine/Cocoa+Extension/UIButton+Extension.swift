85b06d61186547993df07d1a490e090e8eb427f6
ictechgy <ictechgy@gmail.com>
2024-10-29 22:03:06 +0900

    feat: UIButton Binder 작성

---
+++ CodenCombine/Cocoa+Extension/UIButton+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  UIButton+Extension.swift
+//  CodenCombine
+//
+//  Created by JINHONG AN on 10/29/24.
+//
+
+import UIKit
+import Combine
+
+extension CombineReactive where Base: UIButton {
+    public func title(for controlState: UIControl.State = []) -> Binder<String?> {
+        Binder(self.base) { button, title in
+            button.setTitle(title, for: controlState)
+        }
+    }
+
+    public func image(for controlState: UIControl.State = []) -> Binder<UIImage?> {
+        Binder(self.base) { button, image in
+            button.setImage(image, for: controlState)
+        }
+    }
+
+    public func backgroundImage(for controlState: UIControl.State = []) -> Binder<UIImage?> {
+        Binder(self.base) { button, image in
+            button.setBackgroundImage(image, for: controlState)
+        }
+    }
+}
