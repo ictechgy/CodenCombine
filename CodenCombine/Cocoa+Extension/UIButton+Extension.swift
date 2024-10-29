dc1f42271b8065928a83abacbf189c5e4fb590c0
ictechgy <ictechgy@gmail.com>
2024-10-29 22:11:27 +0900

    refactor: UIButton Reactive 접근제어자 수정

--- CodenCombine/Cocoa+Extension/UIButton+Extension.swift
+++ CodenCombine/Cocoa+Extension/UIButton+Extension.swift
@@ -8,26 +8,26 @@
 import UIKit
 import Combine
 
-extension CombineReactive where Base: UIButton {
-    public func title(for controlState: UIControl.State = []) -> Binder<String?> {
+public extension CombineReactive where Base: UIButton {
+    func title(for controlState: UIControl.State = []) -> Binder<String?> {
         Binder(self.base) { button, title in
             button.setTitle(title, for: controlState)
         }
     }
 
-    public func image(for controlState: UIControl.State = []) -> Binder<UIImage?> {
+    func image(for controlState: UIControl.State = []) -> Binder<UIImage?> {
         Binder(self.base) { button, image in
             button.setImage(image, for: controlState)
         }
     }
 
-    public func backgroundImage(for controlState: UIControl.State = []) -> Binder<UIImage?> {
+    func backgroundImage(for controlState: UIControl.State = []) -> Binder<UIImage?> {
         Binder(self.base) { button, image in
             button.setBackgroundImage(image, for: controlState)
         }
     }
     
-    public func attributedTitle(for controlState: UIControl.State = []) -> Binder<NSAttributedString?> {
+    func attributedTitle(for controlState: UIControl.State = []) -> Binder<NSAttributedString?> {
         return Binder(self.base) { button, attributedTitle -> Void in
             button.setAttributedTitle(attributedTitle, for: controlState)
         }
