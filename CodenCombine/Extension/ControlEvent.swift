2a97d221990f7b37d0b7868a1af0deaadab6f3fb
ictechgy <ictechgy@gmail.com>
2024-11-01 02:36:19 +0900

    feat: ControlEvent 생성

--- CodenCombine/Extension/ControlEvent.swift
+++ CodenCombine/Extension/ControlEvent.swift
@@ -5,3 +5,19 @@
 //  Created by JINHONG AN on 10/31/24.
 //
 
+import Combine
+
+public struct ControlEvent<PropertyType> : Publisher {
+    public typealias Output = PropertyType
+    public typealias Failure = Never
+
+    let events: any Publisher<Output, Failure>
+
+    public init<Upstream: Publisher>(events: Upstream) where Upstream.Output == Output, Upstream.Failure == Failure {
+        self.events = events.subscribe(on: MainScheduler.instance)
+    }
+    
+    public func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, PropertyType == S.Input {
+        self.events.receive(subscriber: subscriber)
+    }
+}
