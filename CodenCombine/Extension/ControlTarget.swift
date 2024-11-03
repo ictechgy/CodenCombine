d7f3fef81ae2e5687970bec309ff02a00941ee63
ictechgy <ictechgy@gmail.com>
2024-11-04 00:29:12 +0900

    feat: ControlTarget 구현

--- CodenCombine/Extension/ControlTarget.swift
+++ CodenCombine/Extension/ControlTarget.swift
@@ -5,3 +5,29 @@
 //  Created by JINHONG AN on 11/3/24.
 //
 
+import UIKit
+import Combine
+
+final class ControlTarget: Cancellable {
+    typealias Callback = (UIControl) -> Void
+
+    private var callback: Callback?
+    
+    init(control: UIControl, controlEvents: UIControl.Event, callback: @escaping Callback) {
+        self.callback = callback
+
+        control.addTarget(
+            self,
+            action: #selector(ControlTarget.eventHandler),
+            for: controlEvents
+        )
+    }
+
+    @objc private func eventHandler(_ sender: UIControl) {
+        callback?(sender)
+    }
+    
+    func cancel() {
+        self.callback = nil
+    }
+}
