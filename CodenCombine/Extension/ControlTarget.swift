84ccea0a67456cd3f2a73517092935a83ebc1f1b
ictechgy <ictechgy@gmail.com>
2024-11-06 23:13:24 +0900

    refactor: ControlTarget 구현 형태 수정

--- CodenCombine/Extension/ControlTarget.swift
+++ CodenCombine/Extension/ControlTarget.swift
@@ -8,13 +8,19 @@
 import UIKit
 import Combine
 
-final class ControlTarget: Subscription {
-    typealias Callback = (UIControl) -> Void
-
+final class ControlTarget<Output, S: Subscriber>: Subscription where S.Input == Output, S.Failure == Never {
+    typealias Callback = (UIControl) -> Output
     private var callback: Callback?
+    private var subscriber: S?
     
-    init(control: UIControl, controlEvents: UIControl.Event, callback: @escaping Callback) {
+    init(
+        control: UIControl,
+        controlEvents: UIControl.Event,
+        callback: @escaping Callback,
+        subscriber: S
+    ) {
         self.callback = callback
+        self.subscriber = subscriber
 
         control.addTarget(
             self,
@@ -24,7 +30,8 @@ final class ControlTarget: Subscription {
     }
 
     @objc private func eventHandler(_ sender: UIControl) {
-        callback?(sender)
+        guard let output = callback?(sender) else { return }
+        _ = subscriber?.receive(output) // unlimited
     }
     
     func request(_ demand: Subscribers.Demand) {
@@ -33,5 +40,6 @@ final class ControlTarget: Subscription {
     
     func cancel() {
         self.callback = nil
+        self.subscriber = nil
     }
 }
