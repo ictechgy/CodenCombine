674e7e1d8cb579ec163f68109974f44a62931522
ictechgy <ictechgy@gmail.com>
2024-11-06 23:20:04 +0900

    docs: ControlTarget에 문서화 주석 추가

--- CodenCombine/Extension/ControlTarget.swift
+++ CodenCombine/Extension/ControlTarget.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Combine
 
+/// RxSwift의 ControlTarget에 ControlEvent의 메인쓰레드 보장과 (Subscriber로의) element 직접 전송을 합친 형태
 final class ControlTarget<Output, S: Subscriber>: Subscription where S.Input == Output, S.Failure == Never {
     typealias Callback = (UIControl) -> Output
     private var callback: Callback?
